91d9b0238ae3ac9091aa2055ececed0befdf8a67
Pavan Pitiwaduge <rajivapitiwaduge@gmail.com>
2023-02-20 18:05:47 +0530

    updated model

--- CarImagesWeb/Models/ImageUpload.cs
+++ CarImagesWeb/Models/ImageUpload.cs
@@ -2,11 +2,13 @@
 
 namespace CarImagesWeb.Models
 {
-    public class ImageUpload : TagMapping
+    public class ImageUpload
     {
         public int AssetId { get; set; }
         public Asset Asset { get; set; }
         public string UserId { get; set; }
         public string FileName { get; set; }
+        public int TagId { get; set; }
+        public Tag Tag { get; set; }
     }
 }
