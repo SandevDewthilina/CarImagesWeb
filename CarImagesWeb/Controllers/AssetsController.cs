b52769c18cbbc36b17a0af1870622a098aa84401
Pavan Pitiwaduge <rajivapitiwaduge@gmail.com>
2023-02-24 03:02:31 +0530

    📅 added csv reader

---
+++ CarImagesWeb/Controllers/AssetsController.cs
@@ -0,0 +1,66 @@
+﻿using System.Globalization;
+using System.IO;
+using System.Linq;
+using CarImagesWeb.Models;
+using CarImagesWeb.ViewModels;
+using CsvHelper;
+using Microsoft.AspNetCore.Mvc;
+using OfficeOpenXml;
+
+namespace CarImagesWeb.Controllers
+{
+    public class AssetsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+       
+        [HttpPost]
+        public IActionResult Index(UpdateAssetsViewModel model)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            
+            var fileInput = model.File;
+
+            // Get the file extension
+            var fileExtension = Path.GetExtension(fileInput.FileName);
+
+            switch (fileExtension)
+            {
+                case ".xlsx":
+                {
+                    using var package = new ExcelPackage(fileInput.OpenReadStream());
+                    // Get the first worksheet
+                    var worksheet = package.Workbook.Worksheets.FirstOrDefault();
+                    // Process the data in the worksheet
+                    break;
+                }
+                case ".csv":
+                {
+                    using var reader = new StreamReader(fileInput.OpenReadStream());
+                    using var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
+                    // Read the CSV data into a list of Asset objects
+                    var assets = csv.GetRecords<AssetRecord>().ToList();
+                    // Process the data in the list
+                    break;
+                }
+            }
+
+            // Return a success response
+            return Ok();
+        }
+
+    }
+
+    public class AssetRecord
+    {
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public string Type { get; set; }
+    }
+}
---
+++ CarImagesWeb/ViewModels/UpdateAssetsViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.IO;
+using Microsoft.AspNetCore.Http;
+
+namespace CarImagesWeb.ViewModels
+{
+    public class UpdateAssetsViewModel
+    {
+        [Required]
+        [Display(Name = "Sheet")]
+        [SheetFile]
+        public IFormFile File { get; set; }
+    }
+
+    public class SheetFile : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            // Check if the file is null
+            if (!(value is IFormFile file))
+            {
+                return new ValidationResult("A file is required.");  
+            }
+            // Check if the file extension is .xlsx or .csv
+            var fileExtension = Path.GetExtension(file.FileName);
+            return fileExtension == ".csv" ? ValidationResult.Success  
+                : new ValidationResult("Only csv files are supported.");
+        }
+    }
+}
