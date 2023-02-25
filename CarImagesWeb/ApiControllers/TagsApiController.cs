de99b7e781d6afdeadc25307c3d3bc4fbe669ecd
Pavan Pitiwaduge <rajivapitiwaduge@gmail.com>
2023-02-25 13:15:26 +0530

    🔑 exposed APIs

--- CarImagesWeb/ApiControllers/TagsApiController.cs
+++ CarImagesWeb/ApiControllers/TagsApiController.cs
@@ -1,4 +1,6 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using CarImagesWeb.Helpers;
 using CarImagesWeb.Services;
 using Microsoft.AspNetCore.Authorization;
@@ -18,10 +20,26 @@ namespace CarImagesWeb.ApiControllers
             _tagsHandler = tagsHandler;
         }
 
+        [AllowAnonymous]
         [HttpGet]
         public async Task<IActionResult> GetTags()
         {
             var userRoles = UserHelper.GetRolesOfUser(User);
+            var tags = await _tagsHandler.GetTagsForRoles(userRoles);
+            return Json(new
+            {
+                data = new
+                {
+                    tags
+                }
+            });
+        }
+        
+        [AllowAnonymous]
+        [HttpGet]
+        public async Task<IActionResult> GetTags(List<string> roles)
+        {
+            var userRoles =  roles == null || !roles.Any() ? UserHelper.GetRolesOfUser(User) : roles;
             var tags = await _tagsHandler.GetTagsForRoles(userRoles);
             return Json(new
             {
