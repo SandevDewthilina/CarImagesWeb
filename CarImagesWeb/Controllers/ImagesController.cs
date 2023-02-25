339ca301c845aa49a83068c1020506c964b02727
Pavan Pitiwaduge <rajivapitiwaduge@gmail.com>
2023-02-25 09:46:11 +0530

    🏷⚠️ important yet missed commit

--- CarImagesWeb/Controllers/ImagesController.cs
+++ CarImagesWeb/Controllers/ImagesController.cs
@@ -1,4 +1,6 @@
-﻿using System.Threading.Tasks;
+﻿using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
 using CarImagesWeb.Services;
 using CarImagesWeb.ViewModels;
 using Microsoft.AspNetCore.Authorization;
@@ -24,12 +26,12 @@ namespace CarImagesWeb.Controllers
         [HttpGet]
         public async Task<IActionResult> Upload()
         {
-            //TODO: optimize this
+            var userRole = User.FindFirstValue(ClaimTypes.Role);
             var viewModel = new ImageUploadViewModel
             {
                 Vehicles = await _assetsHandler.GetVehiclesAsync(),
                 Containers = await _assetsHandler.GetContainersAsync(),
-                Tags = await _tagsHandler.GetTagsAsync(),
+                Tags = await _tagsHandler.GetTagsForRole(userRole),
                 CountryCodes = await _countryHandler.GetCountryCodesAsync()
             };
             return View(viewModel);
